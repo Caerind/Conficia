31f2222270b18eafbb86b2557690f5580d37097f
Cmdu76 <charles.mailly@free.fr>
2014-12-15 23:26:28 +0100

    Add Entities.hpp

---
+++ Source/Entities.hpp
@@ -0,0 +1,9 @@
+#ifndef ENTITIES_HPP_INCLUDED
+#define ENTITIES_HPP_INCLUDED
+
+#include "Entities/EntityManager.hpp"
+#include "Entities/Component.hpp"
+#include "Entities/Entity.hpp"
+#include "Entities/System.hpp"
+
+#endif // ENTITIES_HPP_INCLUDED
